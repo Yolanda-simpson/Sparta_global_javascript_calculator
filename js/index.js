710cd597ebf4f4c5d05b8a722b2c39ec5efd8d7c
yolanda <ysimpson@spartaglobal.com>
2019-01-29 11:15:06 +0000

    new changes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-var a = Number(prompt("Enter first number"));
-var operation = (prompt("Enter operation", "+,-,*,/"))
-var b = Number(prompt("Enter second number"));
-var answer = eval(a+operation+b);
+var number_1 = Number(prompt("Enter first number"));
+var operation = prompt("Enter operation", "+,-,*,/");
+var number_2 = Number(prompt("Enter second number"));
+var answer = eval(number_1+operation+number_2);
  alert(answer);
